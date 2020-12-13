f985154f4423697ab0b33c8d0840e68b8a8d58ce
Zaeem K <zaeem.nadeem01@gmail.com>
2020-12-13 23:56:40 +0500

    Add files via upload

---
+++ replaceobject.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+public class replaceobject : EditorWindow
+{
+    public Object source;
+
+    [MenuItem("Ojectediting/Objectreplacment")]
+    static void Init()
+    {
+        var window = GetWindowWithRect<replaceobject>(new Rect(0, 0, 165, 100));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+        source = EditorGUILayout.ObjectField(source, typeof(Object), true);
+        EditorGUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Replace!"))
+        {
+            if (source != null)
+            {
+                foreach (Transform transform in Selection.transforms)
+                {
+                    GameObject g1 = Instantiate(source) as GameObject;
+                    g1.transform.localPosition = transform.localPosition;
+                    g1.transform.localRotation = transform.localRotation;
+
+                    Undo.DestroyObjectImmediate(transform.gameObject);
+                }
+            }
+            else
+                ShowNotification(new GUIContent("No object selected for replacing"));
+        }
+    }
+}
